fb9a941c20941e84e104a26134dcba87564e8981
Sarah S <stms15@mun.ca>
2023-07-30 15:38:09 -0230

    added routes for Tag model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,7 +38,6 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const data = await Category.create({
-      id: req.body.id,
       category_name: req.body.category_name,
     });
     res.status(200).json(data);
